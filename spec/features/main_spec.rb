a6b4d49126619f18021eed55e58cd2155e202274
Dani Wilson <danirae.powell@gmail.com>
2024-03-04 15:10:13 -0700

    edit tests

--- spec/features/main_spec.rb
+++ spec/features/main_spec.rb
@@ -3,14 +3,14 @@ require "rails_helper"
 RSpec.describe "the main page" do
   it "exists" do
     visit "/"
-    save_and_open_page
-    expect(page).to have_css("letter-bank")
+    
+    expect(page).to have_css(".game-image")
   end
 
   it "has a word bank" do
     visit "/"
 
-    expect(page).to have_css("letter-bank")
+    expect(page).to have_css(".letter-bank")
   end
 
   #TO DO
