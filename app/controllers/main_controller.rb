d988f67b78d9686f55e72498359460ac2b7f89ef
Dani Wilson <danirae.powell@gmail.com>
2024-03-04 15:04:47 -0700

    edit controller action

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,4 +2,14 @@ class MainController < ApplicationController
   def index
     @game = Game.new
   end
+
+  def guess
+    letter = params[:letter].downcase
+
+    @guessed_letters << letter
+
+    respond_to do |format|
+      format.js { render 'update_letter_bank' }
+    end
+  end
 end
