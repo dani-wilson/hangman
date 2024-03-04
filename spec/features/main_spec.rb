f66c84cc17d2f09d954f2af299255a50f07af967
Dani Wilson <danirae.powell@gmail.com>
2024-03-04 15:04:20 -0700

    edit testing

--- spec/features/main_spec.rb
+++ spec/features/main_spec.rb
@@ -3,8 +3,20 @@ require "rails_helper"
 RSpec.describe "the main page" do
   it "exists" do
     visit "/"
-
-    expect(page).to have_css("new_game")
+    save_and_open_page
+    expect(page).to have_css("letter-bank")
   end
+
+  it "has a word bank" do
+    visit "/"
+
+    expect(page).to have_css("letter-bank")
+  end
+
+  #TO DO
+  # display letter bank
+  # incorrect guess logic
+  # correct guess logic
+  # , disabled: @guessed_letters.include?(letter) (letter bank)
 end
 
